2ff3065e4c418d8ef109beb3a1dc4b2aa08da388
Joshua Gilo <joshua_gilo@dlsu.edu.ph>
2022-11-02 00:00:12 +0800

    rewrite.

--- main.c
+++ main.c
@@ -1,172 +1,117 @@
 /**
  * @author      : joshuaarmainegilo (joshuaarmainegilo@$HOSTNAME)
  * @file        : main
- * @created     : Monday Oct 31, 2022 20:36:52 PST
+ * @created     : Tuesday Nov 01, 2022 22:45:00 PST
  */
-
+// a modular banking system which implements functions.
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
-struct user{
+
+// function prototypes
+// function to load accounts from struct
+// function to save accounts details to struct
+// functions to transfer money
+// function to check balance
+struct user {
     int id;
     int pin;
     int balance;
 };
 
+void load_data(struct user *user, int *size){
+    //function to load random data into struct
+    struct user accounts[] = {{9999,123456,2000},{9998,123456,2000},{9997,123456,2000},{9996,123456,2000},{9995,123456,2000},{9994,123456,2000},{9993,123456,2000},{9992,123456,2000},{9991,123456,2000},{9990,123456,2000}};
+    *size = sizeof(accounts)/sizeof(accounts[0]);
+    for(int i = 0; i < *size; i++){
+        user[i] = accounts[i];
+    }
+}
 
+int credentialsCheck(int id, int pin,struct user *user, int *size){
+    // function to check credentials
+    for (int i = 0; i < *size; i++){
+        if (id == user[i].id && pin == user[i].pin){
+            return i;
+        }
+    }
+    return -1;
+};
+
+int loginForm(struct user *user, int *size){
+    int id, pin, tries = 0;
+    // limit attempts to 3 tries.
+    printf("Enter your id: ");
+    scanf("%d", &id);
+    printf("Enter your pin: ");
+    scanf("%d", &pin);
+    return credentialsCheck(id, pin, user, size);
+};
+
+void loggedIn(int index, struct user *user, int *size){
+    // function to display menu
+    int choice;
+    printf("1. Check Balance\n 2. Transfer Money\n 3. Exit");
+    scanf("%d", &choice);
+    switch (choice){
+        case 1:
+            printf("Your balance is %d", user[index].balance);
+            break;
+        case 2:
+            //transferMoney(index, user, size);
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice");
+            break;
+    }
+};
+
+int tryLogin(struct user *user, int *size){
+    int id, pin, tries = 0;
+    // limit attempts to 3 tries.
+    while (tries < 3){
+        int index = loginForm(user, size);
+        if (index != -1){
+            printf("Login successful");
+            return index;
+        }
+        else{
+            printf("Login failed. Try again");
+            tries++;
+        }
+    }
+    if (tries == 3){
+        printf("You have exceeded the number of tries. Try again later");
+    }
+    return -1;
+};
+
+void printAllCredentials(struct user *user, int *size){
+    for (int i = 0; i < *size; i++){
+        printf("id: %d, pin: %d, balance: %d", user[i].id, user[i].pin, user[i].balance);
+    }
+};
 
 int main()
 {
-    //struct user user1 = {9999, 123456}, user2 = {8888, 654321}, user3 = {7777, 987654}, user4 = {6666, 456789}, user5 = {5555, 789456}, user6 = {4444, 321987}, user7 = {3333, 147258}, user8 = {2222, 852741}, user9 = {1111, 369852}, user10 = {0000, 258963};
-    struct user accounts[] = {{9999, 123456, 2000}, {8888, 654321, 1000}, {7777, 987654, 1000}, {6666, 456789, 1000}, {5555, 789456, 1000}, {4444, 321987, 1000}, {3333, 147258, 1000}, {2222, 852741, 1000}, {1111, 369852, 1000}, {0000, 258963, 1000}};
-    int username;
-    int pin;
-    int attempts = 0;
-    int withdrawal_money = 1000;
-    int remaining_forex = 3000;
-    while (true){
-        printf("Enter your username: ");
-        scanf("%d", &username);
-        printf("Enter your pin: ");
-        scanf("%d", &pin);
-        if (attempts < 2){
-            for (int i = 0; i < 10; i++){
-                if (username == accounts[i].id && pin == accounts[i].pin){
-                    while (true){
-                        printf("Welcome %d", username);
-                        int choice;
-                        printf("1. Withdrawal\n");
-                        printf("2. Money Transfer\n");
-                        printf("3. Foreign Currency Exchange\n");
-                        printf("4. Bills Payment\n");
-                        printf("5. Online Shop\n");
-                        printf("6. Logout\n");
-                        printf("Enter your choice: ");
-                        scanf("%d", &choice);
-                        if (choice == 1) {
-                            int withdrawal;
-                            printf("Enter the amount you want to withdraw: ");
-                            scanf("%d", &withdrawal);
-                            if (withdrawal > withdrawal_money || withdrawal > accounts[i].balance){
-                                if (withdrawal > withdrawal_money){
-                                    printf("The ATM only has %d", withdrawal_money);
-                                    continue;
-                                }
-                                else if (withdrawal > accounts[i].balance){
-                                    printf("You can only withdraw up to %d", accounts[i].balance);
-                                    continue;
-                                }
-                            }
-                            else {
-                                accounts[i].balance -= withdrawal;
-                                withdrawal_money -= withdrawal;
-                                printf("Remaining balance: %d", accounts[i].balance);
-                                continue;
-                            }
-                        } else if (choice == 2) {
-                            int transfer, account_transfer;
-                            printf("Enter the account number you want to transfer: ");
-                            scanf("%d", &account_transfer);
-                            printf("Enter the amount you want to transfer: ");
-                            scanf("%d", &transfer);
-                            if (transfer > accounts[i].balance){
-                                printf("You can only transfer up to %d", accounts[i].balance);
-                                continue;
-                            }
-                            else {
-                                accounts[i].balance -= transfer;
-                                for (int j = 0; j < 10; j++){
-                                    if (account_transfer == accounts[j].id){
-                                        accounts[j].balance += transfer;
-                                        printf("Remaining balance: %d", accounts[i].balance);
-                                        printf("Account %d balance: %d", accounts[j].id, accounts[j].balance);
-                                        continue;
-                                    }
-                                }
-                            }
-                        } else if (choice == 3){
-                            float usd = 58.4060;
-                            float jpy = 0.4104;
-                            float gbp = 65.7418;
-                            float hkd = 7.4413;
-                            float chf = 59.7809;
 
-                            int currency;
-                            float tax = 0.12;
-                            printf("1. USD\n 2. JPY\n 3. GBP\n 4. HKD\n 5. CHF\n");
-                            printf("Enter the currency you want to exchange: ");
-                            scanf("%d", &currency);
-                            if (currency == 1){
-                                int amount;
-                                printf("Enter the amount you want to exchange: ");
-                                scanf("%d", &amount);
-                                if (amount > remaining_forex || amount > accounts[i].balance){
-                                    if (amount > remaining_forex){
-                                        printf("The ATM only has %d", remaining_forex);
-                                        continue;
-                                    }
-                                    else if (amount > accounts[i].balance){
-                                        printf("You can only exchange up to %d", accounts[i].balance);
-                                        continue;
-                                    }
-                                }
-                                else {
-                                    accounts[i].balance -= amount;
-                                    remaining_forex -= amount;
-                                    float total = amount / usd;
-                                    float total_tax = total * tax;
-                                    float total_amount = total + total_tax;
-                                    printf("Total amount: %f", total_amount);
-                                    continue;
-                                } 
-                            } else if (currency == 2){
-                                int amount;
-                                printf("Enter the amount you want to exchange: ");
-                                scanf("%d", &amount);
-                                if (amount > remaining_forex || amount > accounts[i].balance){
-                                    if (amount > remaining_forex){
-                                        printf("The ATM only has %d", remaining_forex);
-                                        continue;
-                                    }
-                                    else if (amount > accounts[i].balance){
-                                        printf("You can only exchange up to %d", accounts[i].balance);
-                                        continue;
-                                    }
-                                }
-                                else {
-                                    accounts[i].balance -= amount;
-                                    remaining_forex -= amount;
-                                    float total = amount / jpy;
-                                    float total_tax = total * tax;
-                                    float total_amount = total + total_tax;
-                                    printf("Total amount: %f", total_amount);
-                                    continue;
-                                }
-                            }
-                        } else if (choice == 4) {
-                        
-                            int bill;
-                            // cebu pacific, philcare, and paramount life are available for all users
-                            // toyota financial services, bankard, rcbc, mastercard, and city loft are only available for account numbers which are prime numbers.
-                            // unionbank, and unistar are only available for account numbers which are divisible by 4
-                            // hsbc personal loans, easytrip, psbank loans and pldt are only available for account numbers which are in the fibonacci sequencoe
-                            int choice;
-                            int user_bills[3];
-                            if (accounts[i].id % 4 == 0){
-                                user_bills[0] = 1;
-                            }
-
-                        }
-                    }
-                } else if (i == 9){
-                    printf("Invalid username or pin\n");
-                    attempts++;
-                }
-            }
-        } else {
-            printf("Too many failed attempts");
-            break;
-        }
+    int size = 10;
+    struct user accounts[size];
+    load_data(accounts, &size);
+    printAllCredentials(accounts, &size);
+    int index = tryLogin(accounts, &size);
+    while (index != -1){
+        loggedIn(index, accounts, &size);
     }
+
     return 0;
 }
 
